0bcb393176fe60c64a8b43f3c347e1080558597c
Carnage <akotoxmpimbo@gmail.com>
2025-04-17 17:16:04 +0200

    Remove unused variable `utcToSend` in MeetingForm component

--- src/components/forms/MeetingForm.tsx
+++ src/components/forms/MeetingForm.tsx
@@ -114,7 +114,6 @@ export function MeetingForm({
         zone: form.watch("timezone"),
       }
     );
-    const utcToSend = bangkokTime.toUTC().toISO();
 
     const data = await createMeeting({
       ...values,
